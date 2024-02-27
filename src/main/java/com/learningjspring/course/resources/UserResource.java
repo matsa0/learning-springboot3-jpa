9d318c71f5b556e201fb51f7026551ac2525b73a
matsa0 <az3vedomatheus@gmail.com>
2024-02-27 20:37:16 -0300

    Delete User @DeleteMapping noContent

--- src/main/java/com/learningjspring/course/resources/UserResource.java
+++ src/main/java/com/learningjspring/course/resources/UserResource.java
@@ -3,14 +3,14 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -49,8 +49,13 @@ public class UserResource {
         user = userService.insertUser(user);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
     
-        return ResponseEntity.created(uri).body(user);
+        return ResponseEntity.created(uri).body(user); //Retornando a resposta HTTP 201 CREATED
     }
     
+    @DeleteMapping(value = "/{id}") //notação padrão REST deleção HTTP
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build(); //resposta que não tem conteúdo(Já retorna a resposta HTTP correta)
+    }
 }
 
